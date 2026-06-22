4de9db1f80a09e7602012a8673390e6f8a57e311
[Buchik] <[yaBuchik@yandex.ru]>
2026-06-22 14:52:35 +0300

    made ordering by id for displaying users

--- task_manager/task_manager/models.py
+++ task_manager/task_manager/models.py
@@ -6,3 +6,6 @@ class Users(models.Model):
     surname = models.CharField(max_length=200, blank=True)
     nickname = models.CharField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["id"]
