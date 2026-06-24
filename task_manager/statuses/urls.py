baf47961dbc0eab4c77ab7dd3e5b0ec27fa8abd5
[Buchik] <[yaBuchik@yandex.ru]>
2026-06-25 00:44:14 +0300

    created list of statuses page

--- task_manager/statuses/urls.py
+++ task_manager/statuses/urls.py
@@ -6,7 +6,7 @@ from . import views
 app_name = 'statuses'
 
 urlpatterns = [
-    #path("", views.StatusesIndexView.as_view(), name="list"),
+    path("", views.StatusesIndexView.as_view(), name="list"),
     #path("create/", , name="create"),
     #path("<int:pk>/update/", , name="update"),
     #path("<int:pk>/delete/", , name="delete"),
--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.views import View
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.utils.translation import gettext_lazy as _
 
-# Create your views here.
+from .models import Status
+
+class StatusesIndexView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        statuses = Status.objects.all()
+        return render(request, "statuses_list.html", {"statuses": statuses})
