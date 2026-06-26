0a32279147b1977356ccb8ac025b70f46cf6ecd4
[Buchik] <[yaBuchik@yandex.ru]>
2026-06-26 16:29:38 +0300

    deleted wrong condition for name

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=200, blank=True, unique=True)
+    name = models.CharField(max_length=200, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
