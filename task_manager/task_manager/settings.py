c120c8dfe0bcd58a8981e14846711b116108bbe4
[Buchik] <[yaBuchik@yandex.ru]>
2026-06-29 17:58:58 +0300

    changed Secret_key and debug

--- task_manager/task_manager/settings.py
+++ task_manager/task_manager/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zvmpxjj4wb)!ayu772bh^fiv(w%e4akdnvor6fgu-3m&=jow76'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = []
 
@@ -82,11 +87,7 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-import dj_database_url
-from dotenv import load_dotenv
-import os
 
-load_dotenv()
 
 
 if os.environ.get('DJANGO_DB_HOST'):
