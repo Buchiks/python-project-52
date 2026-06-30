ade61aeccb18eca5564c5811a9f6067196bdd25c
[Buchik] <[yaBuchik@yandex.ru]>
2026-07-01 01:26:10 +0300

    changed delete view

--- labels/views.py
+++ labels/views.py
@@ -1,13 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import render
+from django.db.models.deletion import ProtectedError
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.utils.translation import gettext_lazy as _
 from django.views import View
 from django.views.generic import CreateView, DeleteView, UpdateView
 
 from .models import Label
-from .utils import LabelHasTasksTestMixin
 
 
 class LabelsListView(LoginRequiredMixin, View):
@@ -47,14 +47,17 @@ class LabelUpdateView(LoginRequiredMixin, UpdateView):
         return response
 
 
-class LabelDeleteView(LabelHasTasksTestMixin, LoginRequiredMixin, DeleteView):
+class LabelDeleteView(LoginRequiredMixin, DeleteView):
 
     model = Label
-    fields = ["name"]
     template_name = "labels/delete.html"
     success_url = reverse_lazy("labels:list")
 
     def form_valid(self, form):
+        label = self.get_object()
+        if label.tasks.exists():
+            messages.warning(self.request, _("Cannot delete label"))
+            return redirect("labels:list")
         response = super().form_valid(form)
         messages.success(self.request, _('Label successfully deleted'))
         return response
