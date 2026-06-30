5b1942c75376519f4dc24c967140698a829f7fc6
[Buchik] <[yaBuchik@yandex.ru]>
2026-06-30 20:22:29 +0300

    added password conformation in user update

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm, UserChangeForm, UserCreationForm
 from django.utils.translation import gettext_lazy as _
 from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Users
 
@@ -29,6 +30,12 @@ class UserUpdateForm(UserChangeForm):
         required=False,
         help_text=_("Leave blank if you don't want to change it")
     )
+    password_confirm = forms.CharField(
+        label=_("Password conformation"),
+        widget=forms.PasswordInput,
+        required=False,
+        help_text=_("Enter the same password again")
+    )
 
     class Meta:
         model = Users
@@ -42,6 +49,19 @@ class UserUpdateForm(UserChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
     
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        password_confirm = cleaned_data.get('password_confirm')
+        
+        if password or password_confirm:
+            if password != password_confirm:
+                raise ValidationError({
+                    'password_confirm': _("Passwords do not match")
+                })
+            
+        return cleaned_data
+            
     def save(self, commit=True):
         user = super().save(commit=False)
         password = self.cleaned_data.get('password')
